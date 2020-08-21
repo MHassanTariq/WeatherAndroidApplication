4d6a557243e4f9864457f95b653af469c6c2b12b
MHassanTariq <h.tariq10000@gmail.com>
2020-08-21 16:28:08 +0500

    Implement Glide

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/Utils.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapp
+
+import android.util.Log
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    var myobject = imgUrl?.let {
+        val imgUri =
+            imgUrl.toUri().buildUpon().scheme("https").build()
+        Log.d("Utils", "imgUri : $imgUri")
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.very_sunny)
+                .error(R.drawable.error_img_loading))
+            .into(imgView)
+    }
+    Log.d("Utils", "$myobject")
+}
